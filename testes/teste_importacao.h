0be87b92f7541f273dfa655780c787617435e9f8
RafaelRSoares <rafasoares159@gmail.com>
2026-06-27 18:47:55 -0300

    Execução de comandos e interface
    Gostaria que mais alguém tentasse rodar antes pra ver como ficou e pra confirmar que está funcionando em mais de um pc.

--- testes/teste_importacao.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTE_IMPORTACAO_H
-#define TESTE_IMPORTACAO_H
-
-#include "../src/disco/disco.h"
-#include "../src/i-node/inode.h"
-
-void testImportacao(VirtualDisk *disk, Inode inodeTable[], int rootInode);
-
-#endif
